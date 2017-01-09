3400d75cce650b8f5e74d86a646e185a95307070
Ekaterina Loshchinina <KateLoschinina@Ekaterinas-MacBook-Pro.local>
2017-01-09 17:12:35 +0000

    Created a deduct method for Oystercard class

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,12 +1,15 @@
 class Oystercard
 
   MAX_BALANCE = 90
+  MIN_BALANCE = -5
 
-  def initialize(amount = 0, max_balance = MAX_BALANCE)
+  def initialize(amount = 0, min_balance = MIN_BALANCE, max_balance = MAX_BALANCE)
     @balance = amount
+    @min_balance = min_balance
     @max_balance = max_balance
   end
 
+  attr_accessor :min_balance
   attr_accessor :max_balance
   attr_reader :balance
 
@@ -14,6 +17,12 @@ class Oystercard
     error_message = "Your card's balance cannot exceed £#{@max_balance}."
     raise error_message if @balance + amount > @max_balance
     @balance += amount
+  end
+
+  def deduct(amount)
+    error_message = "Your card's balance can't go below £#{@min_balance}."
+    raise error_message if @balance - amount < @min_balance
+    @balance -= amount
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,6 +6,9 @@ describe Oystercard do
   subject(:card1) { described_class.new }
   subject(:card2) { described_class.new init_amount }
 
+  maximum_balance = Oystercard::MAX_BALANCE
+  minimum_balance = Oystercard::MIN_BALANCE
+
   describe 'class exists' do
     it 'checks whether creates an instance of a class' do
       expect(card1).to be_a(Oystercard)
@@ -23,16 +26,26 @@ describe Oystercard do
   end
 
   describe 'top_up' do
-    it {is_expected.to respond_to(:top_up).with(1).argument}
+    #it {is_expected.to respond_to(:top_up).with(1).argument}
     it 'top_ups by the number' do
       number = 10
-      card1.top_up(number)
-      expect(card1.balance).to eq(number)
+      expect{ card1.top_up(number) }.to change{ card1.balance }.by number
     end
-    it 'stops user from having a balance beyond £90' do
-      maximum_balance = Oystercard::MAX_BALANCE
+    it "stops user from having a balance beyond £#{maximum_balance}" do
       error_message = "Your card's balance cannot exceed £#{maximum_balance}."
       expect {card1.top_up(maximum_balance + 1)}.to raise_error (error_message)
+    end
+  end
+
+  describe 'deduct' do
+    #it {is_expected.to respond_to(:deduct).with(1).argument}
+    it 'duducts by the number' do
+      number = 3
+      expect{ card1.deduct number }.to change{ card1.balance }.by -number
+    end
+    it "stops user from having a balance below £#{minimum_balance}" do
+      error_message = "Your card's balance can't go below £#{minimum_balance}."
+      expect {card1.deduct(-minimum_balance + 1)}.to raise_error(error_message)
     end
   end
 end
